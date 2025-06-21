c7c0f1b86ed40b226098d9deef1f119becf037e5
Gordy <gordy2007@hotmail.co.uk>
2025-06-21 10:47:44 +0100

    Update 01-install-copr-repos-dx.sh

--- build_files/dx/01-install-copr-repos-dx.sh
+++ build_files/dx/01-install-copr-repos-dx.sh
@@ -31,4 +31,7 @@ dnf5 -y copr enable hikariknight/looking-glass-kvmfr
 # Podman-bootc
 dnf5 -y copr enable gmaglione/podman-bootc
 
+# Ghostty
+dnf5 -y copr enable pgdev/ghostty
+
 echo "::endgroup::"
